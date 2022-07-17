d4364fc39338f5d068d4fb097f13e505416ab4ab
dan md <lucrce kabaka@danmpinda.local>
2022-07-17 23:19:19 +0300

    commit 4

--- 02_Day_Data_types/math_object.js
+++ 02_Day_Data_types/math_object.js
@@ -16,7 +16,7 @@ console.log(num)
 //Absolute value
 console.log(Math.abs(-10)) //10
 //Square root
-console.log(Math.sqrt(100)) // 10
+console.log(Math.sqrt(1000)) // 10
 console.log(Math.sqrt(2)) //1.4142135623730951
 // Power
 console.log(Math.pow(3, 2)) // 9
